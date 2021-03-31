45e93003c818cf6159b892dffe41087eb9debfbd
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-03-31 11:07:57 -0400

    remove rounded border and margin bottom of the middle input

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var userFeed = new Instafeed({
-  get: "user",
-  target: "instafeed-container",
-  resolution: "low_resolution",
-  accessToken:
-    "IGQVJYd3l4U05OTDNha29yUDdYMTNfZA0I3ZAGk1ZAFVzZA3QtNUxyRTMxaXpjbTJfYVo0WEctY1ZAvV1g5bS1FRFloSy1VQklfVGVPS21NRTlDT0tEdUJUX2pnQjUwU1VzMV9TN0FzLVNvaVBWOVp3bFVyaQZDZD",
-});
-userFeed.run();
+// var userFeed = new Instafeed({
+//   get: "user",
+//   target: "instafeed-container",
+//   resolution: "low_resolution",
+//   accessToken:
+//     "IGQVJYd3l4U05OTDNha29yUDdYMTNfZA0I3ZAGk1ZAFVzZA3QtNUxyRTMxaXpjbTJfYVo0WEctY1ZAvV1g5bS1FRFloSy1VQklfVGVPS21NRTlDT0tEdUJUX2pnQjUwU1VzMV9TN0FzLVNvaVBWOVp3bFVyaQZDZD",
+// });
+// userFeed.run();
